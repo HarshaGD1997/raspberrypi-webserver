c83a6d5f70743f03ac89751a27d62d001f28d95b
HarshaGD1997 <harshagd695@gmail.com>
2025-09-21 18:23:57 +0530

    Echo web server

--- src/module1_echo_server/echo_server.cpp
+++ src/module1_echo_server/echo_server.cpp
@@ -57,6 +57,69 @@ int main(){
 	std::cout << "Echo server listening on port : "<< port << " ..." << std::endl;
 
 
+	//accept loop (blocking) one client at a time.
+	
+	while(true) {
+		
+		sockaddr_in client{};	
+		socklen_t len = sizeof(client);
+		int client_fd = accept(listen_fd, reinterpret_cast<sockaddr*>(&client), &len);
+		if(client_fd < 0){
+			if(errno == EINTR) 
+				continue; //Interrupted by signal, retry
+			perror("accept");
+			break;
+		}
+		
+		char ipstr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &client.sin_addr, ipstr, sizeof(ipstr));
+		std::cout << "Client connected from " <<ipstr << ":" << ntohs(client.sin_port) << std::endl;
 
+
+		//echo loop: read -> write_back -> repeat until client closes.	
+		
+		char buf[4096];
+		while(true) {
+			ssize_t n = read(client_fd, buf, sizeof(buf));
+
+			if(n == 0){
+				std::cout << "Client closed connection" << std::endl;
+				break;
+			
+			}
+
+			if(n < 0){
+				if(errno == EINTR) 
+					continue;
+				perror("read");
+				break;
+			
+			}
+
+			//Write the same bytes back
+			
+			ssize_t to_write = n, written = 0;
+			while (written < to_write) {
+				ssize_t w = write(client_fd, buf + written, to_write - written);
+				if (w < 0){
+					if(errno == EINTR)
+						continue;
+					perror("write");
+					goto done_with_client;
+				}
+
+				written += w;
+			
+			}
+		
+		
+		}
+
+		done_with_client:
+			close(client_fd);
+
+	}
+
+	close(listen_fd);
 	return 0;
 }
